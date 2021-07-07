0f06fe1164bb6b83303ed776876a39965c0cc9b8
Brezus <73348763+Brezus@users.noreply.github.com>
2021-07-07 02:19:54 -0400

    first repo!
    will have to change certain links in order for this website to load properly on github pages

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var emailForm = document.getElementById("email-form");
+
+function emailValidate() {
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        isEmailValid();
+}
