ed4c2a3a33e6cfffae4a28887ff5ad2b23c381a9
Brezus <73348763+Brezus@users.noreply.github.com>
2021-07-07 20:15:05 -0400

    fixing even more bugs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-var emailForm = document.getElementById("email-form");
-
-function emailValidate() {
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        isEmailValid();
-}
+const form = document.getElementById("form");
+const email = document.getElementById("email");
