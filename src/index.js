eef71972472f50b2dec98a12f585485bdc649bda
Brezus <73348763+Brezus@users.noreply.github.com>
2021-07-08 01:11:46 -0400

    sigh

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
-const form = document.getElementById("form");
+const form = document.querySelector("#form");
 const email = document.getElementById("email");
+const errorIcon = document.querySelector(".error-icon");
+const errorText = document.querySelector(".error-text");
+const fail = document.getElementsByClassName("email");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInput();
+});
+function checkInput() {
+  errorIcon.classList.add("hidden");
+  if (!email.value) {
+    errorIcon.classList.remove("hidden");
+    errorText.classList.remove("hidden");
+    fail.classList.add(".fail");
+  }
+  if (!isEmail(email)) {
+    errorIcon.classList.remove("hidden");
+    errorText.classList.remove("hidden");
+  }
+  if (isEmail(email)) {
+    errorIcon.classList.add("hidden");
+    errorText.classList.add("hidden");
+  }
+}
+
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
